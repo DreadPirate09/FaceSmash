37a62ba8efd41072195829d1fa4afcc4badd1ac9
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2023-01-23 00:10:45 +0200

    v1-start looking into the bug

--- face_smash/lib/counter.dart
+++ face_smash/lib/counter.dart
@@ -2,8 +2,13 @@ class Counter {
   Counter._();
   static final instance = Counter._();
   static int counter = 0;
+  static int max_member = 18;
   Future<void> incrementCounter() async {
-    counter++;
+    if (counter == max_member) {
+      counter = 0;
+    } else {
+      counter++;
+    }
   }
 
   Future<void> decrementCounter() async {
